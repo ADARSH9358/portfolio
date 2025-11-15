b1987321b86500f1a56b11c3f7b914ffe9e12dd8
Adarsh kumar <122091788+ADARSH9358@users.noreply.github.com>
2025-11-15 23:53:59 +0530

    fade

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -38,7 +38,7 @@ export default function Skills() {
         {/* <BackgroundGradientAnimation>
             <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl"></div>
           </BackgroundGradientAnimation> */}
-        <Fade right duration={1000}>
+        <Fade direction="right" duration={1000} triggerOnce>
           <div className="skills-text-div">
             <h1
               className={isDark ? "dark-mode skills-heading" : "skills-heading"}
